4ac8d43fad65e0ccae0ab6f168c2fff404f4da78
xyp8023 <xyp8023@gmail.com>
2018-12-31 13:12:34 +0100

    Create 950_Reveal_Cards_In_Increasing_order.py

---
+++ 950_Reveal_Cards_In_Increasing_order.py
@@ -0,0 +1,19 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: List[int]
+        """
+        deck = sorted(deck)
+        r_deck = [deck[-1]]
+        for i in range(len(deck)-2,-1,-1):
+            r_deck.insert(0, r_deck.pop())
+            r_deck.insert(0, deck[i])
+
+        return r_deck
+
+
+deck = [17, 13, 11, 2, 3, 5, 7]
+sol = Solution()
+res = sol.deckRevealedIncreasing(deck)
+print(res)
